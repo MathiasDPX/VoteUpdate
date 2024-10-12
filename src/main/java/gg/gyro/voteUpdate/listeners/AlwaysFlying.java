314082c821e82d189218d06264affcb351658f2a
MathiasDPX <mathias@dupeux.net>
2024-10-12 22:03:16 +0200

    Simplify

--- src/main/java/gg/gyro/voteUpdate/listeners/AlwaysFlying.java
+++ src/main/java/gg/gyro/voteUpdate/listeners/AlwaysFlying.java
@@ -1,6 +1,5 @@
 package gg.gyro.voteUpdate.listeners;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -8,11 +7,6 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class AlwaysFlying implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        if (!player.isOnGround()) {
-            player.setGliding(true);
-        } else {
-            player.setGliding(false);
-        }
+        event.getPlayer().setGliding(true);
     }
 }
