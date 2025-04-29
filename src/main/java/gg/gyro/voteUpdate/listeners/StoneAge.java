64d0c468b5ad8b996a4cd7c3d0a6bd135ff23621
MathiasDPX <mathias@dupeux.net>
2025-04-29 22:11:09 +0200

    add 'Stone age'

---
+++ src/main/java/gg/gyro/voteUpdate/listeners/StoneAge.java
@@ -0,0 +1,18 @@
+package gg.gyro.voteUpdate.listeners;
+
+import gg.gyro.localeAPI.Locales;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class StoneAge implements Listener {
+    @EventHandler
+    void onJoin(PlayerJoinEvent event) {
+        Component playername = Component.text(Locales.getInstance().get("options.stone_age.playername"));
+        Player player = event.getPlayer();
+        player.playerListName(playername);
+        player.displayName(playername);
+    }
+}
---
+++ src/main/java/gg/gyro/voteUpdate/votes/StoneAge.java
@@ -0,0 +1,37 @@
+package gg.gyro.voteUpdate.votes;
+
+import gg.gyro.localeAPI.Locales;
+import gg.gyro.voteUpdate.VoteUpdate;
+import gg.gyro.voteUpdate.utils.Vote;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class StoneAge extends Vote {
+    @Override
+    public ItemStack getIcon() {
+        return new ItemStack(Material.STONE);
+    }
+
+    @Override
+    public String getName() {
+        return Locales.getInstance().get("options.stone_age.name");
+    }
+
+    @Override
+    public String getDescription() {
+        String playername = Locales.getInstance().get("options.stone_age.playername");
+        return Locales.getInstance().get("options.stone_age.description").replace("%playername%", playername);
+    }
+
+    @Override
+    public void apply() {
+        VoteUpdate.registerEvents(new gg.gyro.voteUpdate.listeners.StoneAge());
+        Component playername = Component.text(Locales.getInstance().get("options.stone_age.playername"));
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.displayName(playername);
+        }
+    }
+}
