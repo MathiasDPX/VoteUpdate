c56a716f71a4d65a1e3f363bb7797300bc345ceb
MathiasDPX <mathias@dupeux.net>
2025-04-29 20:36:30 +0200

    Add "hot_potatoes"

---
+++ src/main/java/gg/gyro/voteUpdate/votes/HotPotatoes.java
@@ -0,0 +1,36 @@
+package gg.gyro.voteUpdate.votes;
+
+import gg.gyro.localeAPI.Locales;
+import gg.gyro.voteUpdate.VoteUpdate;
+import gg.gyro.voteUpdate.utils.Vote;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class HotPotatoes extends Vote {
+    @Override
+    public ItemStack getIcon() {
+        return new ItemStack(Material.BAKED_POTATO);
+    }
+
+    @Override
+    public String getName() {
+        return Locales.getInstance().get("options.hotpotatoes.name");
+    }
+
+    @Override
+    public String getDescription() {
+        return Locales.getInstance().get("options.hotpotatoes.description");
+    }
+
+    @Override
+    public void apply() {
+        Bukkit.getScheduler().runTaskTimer(VoteUpdate.getInstance(), () -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                if (player.getInventory().contains(Material.BAKED_POTATO)) {
+                    player.damage(1);
+                }
+            });
+        }, 0, 20);
+    }
+}
